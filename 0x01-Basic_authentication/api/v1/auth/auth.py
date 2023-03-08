1b375256c5caeaa7d51b96ea7b33251e8b9753e9
Marvin Kurland <bugemarvin@outlook.com>
2023-03-08 09:13:10 +0300

    checking false and true

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,6 +21,8 @@ class Auth:
         for pwds in excluded_paths:
             if path in pwds:
                 return False
+            else:
+                return True
         return True
 
     def authorization_header(self, request=None) -> str:
