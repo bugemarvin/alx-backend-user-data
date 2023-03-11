4f395d9d71f75b40ee4b9d0b1bbdf0a102bc9284
Marvin Kurland <bugemarvin@outlook.com>
2023-03-11 11:22:02 +0300

    creating class basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,10 +12,14 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+basic_auth = None
 auth = environ.get('AUTH_TYPE')
 if auth:
     from api.v1.auth.auth import Auth
     auth = Auth()
+if auth == basic_auth:
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Basic auths
+'''
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    '''inherit from class Auth
+    '''
+    pass
