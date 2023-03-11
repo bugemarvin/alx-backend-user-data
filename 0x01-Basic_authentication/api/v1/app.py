c76120562023f2c67b702821e8cd08192aee340a
Marvin Kurland <bugemarvin@outlook.com>
2023-03-11 11:40:36 +0300

    fixes for auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,11 +12,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth = environ.get('AUTH_TYPE')
-if auth:
+basic_auth = None
+AUTH_TYPE = environ.get('AUTH_TYPE')
+if AUTH_TYPE == auth:
     from api.v1.auth.auth import Auth
     auth = Auth()
-if auth == 'basic_auth':
+if AUTH_TYPE == basic_auth:
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
