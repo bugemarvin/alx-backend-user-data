6358b2a6048df43635d33ca2b8b5ac968929c83e
Marvin Kurland <bugemarvin@outlook.com>
2023-03-15 18:21:30 +0300

    mapping variables for sql type and thier size

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''creating a model of users
+'''
+from sqlalchemy import Integer, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    '''
+        - Model for users
+        Return:
+                - users
+                - users.id: INTEGER
+                - users.email: VARCHAR(250)
+                - users.hashed_password: VARCHAR(250)
+                - users.session_id: VARCHAR(250)
+                - users.reset_token: VARCHAR(250)
+    '''
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=False)
